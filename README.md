d30d32b47f065b547178706128a888f96e15fba5
Romulo Britta <romulo.britta@codeitsolutions.com.br>
2018-08-22 17:40:15 -0300

    Ajustes para deploy

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-
-
 References
 
 Translation: https://github.com/ngx-translate/core
 Charts: http://www.chartjs.org/docs/latest/charts
+
+# Deploy
+see cordapp-demo-backend / README.md
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiUrlInsurer: 'http://10.49.248.31:10007/api',
+  apiUrlChannel: {
+    1: 'http://10.49.248.31:10010/api',
+    2: 'http://10.49.248.31:10013/api'
+  },
+  languages: ['en-US', 'pt-BR'],
+  language: 'pt-BR'
+};
+
